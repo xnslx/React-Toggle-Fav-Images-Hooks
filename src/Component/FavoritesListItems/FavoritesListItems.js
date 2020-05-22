1f2a830d739b10f1427facac21042a7b80ff83b2
Xian Li <xnslx89@gmail.com>
2020-05-21 23:17:22 -0500

    delete image

--- src/Component/FavoritesListItems/FavoritesListItems.js
+++ src/Component/FavoritesListItems/FavoritesListItems.js
@@ -1,13 +1,24 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ImageContext} from '../Store/Store';
 import classes from './FavoritesListItems.module.css';
 
 const FavoritesListItems = (props) => {
-    const {state} = useContext(ImageContext)
+    const {state } = useContext(ImageContext)
     console.log(state)
     const {favoritesList} = state;
     console.log(favoritesList)
     
+    const buttonStyle = {
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+    const [favImage, setFavImage] = useState(favoritesList)
+
+    const deleteImageHandler = (index) => {
+        const updatedList = favoritesList.splice(index,1)
+        setFavImage({favImage: updatedList})
+    }
     return (
         <div className={classes.FavList} >
             {favoritesList.map(item => (
@@ -18,6 +29,9 @@ const FavoritesListItems = (props) => {
                         style={{width: '80vw' }}
                     />
                     <li style={{listStyle: 'none', fontSize: '18px', textAlign:'center', marginTop:'8px'}}>{item.title}</li>
+                    <div style={buttonStyle}>
+                        <button onClick={() => deleteImageHandler(item.index)}>Delete</button>
+                    </div>
                 </ul>
             ))}
         </div>
