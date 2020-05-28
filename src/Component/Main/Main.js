dbe9e49b77a819b7bdf0bcdf9934da0da50465b7
Xian Li <xnslx89@gmail.com>
2020-05-27 21:48:05 -0500

    modify

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
         <div>
             <Switch>
                 <Route path='/favImages' component={FavoritesListItems}/>
-                <Route path='/images' component={Images}/>
+                <Route path='/' component={Images}/>
             </Switch>
         </div>
     )
